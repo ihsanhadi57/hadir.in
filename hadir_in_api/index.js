2c2afa74d84fabbd025d4cb69e12b01162f7121d
Ihsan Hadi <ihsanhadi57@gmail.com>
2026-04-21 23:38:38 +0700

    fix index js for deploy

--- hadir_in_api/index.js
+++ hadir_in_api/index.js
@@ -1,3 +1,13 @@
+process.on('uncaughtException', (err) => {
+    console.error('UNCAUGHT EXCEPTION:', err)
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (reason) => {
+    console.error('UNHANDLED REJECTION:', reason)
+    process.exit(1)
+})
+
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
